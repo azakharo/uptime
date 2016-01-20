f216466f0371d4540e002e23f760c24f708ccc31
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-01-20 13:37:27 +0300

    Initial commit.

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/armuptime-dev'
+  },
+
+  seedDB: true
+};
