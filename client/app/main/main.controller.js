58bd510d75cd8bf09e1a2a06d8f046b9bfda00fa
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-01-21 11:33:55 +0300

    Introduced the time period selector.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,7 @@
 angular.module('armUptimeApp')
   .controller('MainCtrl', function ($scope, $log, $state, Auth) {
     $scope.Auth = Auth;
+    $scope.timePeriod = 'day';
 
     $scope.onSettingsBtnClick = function() {
       log("settings clicked");
