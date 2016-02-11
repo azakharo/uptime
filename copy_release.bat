b42d93e1634443950a7e17b43c09f53d019fd868
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-02-11 13:48:21 +0300

    Updated the copy release script.

--- copy_release.bat
+++ copy_release.bat
@@ -2,10 +2,12 @@ SET TRG=arm-uptime-release
 
 pushd ..\\%TRG%
 git rm -r app
-git rm index.html
+git rm -r components
 popd
 
 pushd dist\\public
 cp -R app ../../../%TRG%
+cp -R components ../../../%TRG%
+cp -R bower_components ../../../%TRG%
 cp index.html ../../../%TRG%
 popd
